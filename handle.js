81cd3951945523d08bdac5637a223e15e3a90072
at-tannguyen2 <tan.nguyen2@asiantech.vn>
2020-06-16 22:31:23 +0700

    excersice

--- handle.js
+++ handle.js
@@ -1,15 +1,27 @@
-function addToCart(obj) {
-  var arrProduct = localStorage.getItem('arrProduct');
-  arrProduct = arrProduct ? JSON.parse(arrProduct) : [];
-  var parent = obj.parentNode;
-  var url_image = parent.children[0].src;
-  var price = parent.children[1].textContent;
-  objProduct = {
-    url_image,
-    price
-  }
-  arrProduct.push(objProduct);
-  localStorage.setItem('arrProduct', JSON.stringify(arrProduct))
-  console.log(url_image, price);
+var btnRegister = document.getElementById('js-register');
+btnRegister.addEventListener('click', function () {
+  var nameValue = document.getElementById('inputName').value;
+  var ageValue = document.getElementById('inputAge').value;
+  var addressValue = document.getElementById('inputAddress').value;
+  var objStudent = {
+    nameValue,
+    ageValue,
+    addressValue
+  };
+  var arrStudent = localStorage.getItem('arrStudent');
+  arrStudent = arrStudent ? JSON.parse(arrStudent) : [];
+  arrStudent.push(objStudent);
+  localStorage.setItem('arrStudent', JSON.stringify(arrStudent));
+  renderStudent();
+})
+function renderStudent() {
+  var arrStudent = JSON.parse(localStorage.getItem('arrStudent'));
+  console.log(arrStudent);
+  var renderArea = document.getElementById('js-students');
+  console.log(renderArea);
+  var content = arrStudent.map(function (item, index) {
+    return '<tr><th>' + parseInt(index + 1) + '</th><th>' + item.nameValue + '</th><th>' + item.ageValue + '</th><th>' + item.addressValue + '</th><tr>';
+  })
+  renderArea.innerHTML = content.join('');
 }
-
+renderStudent();
